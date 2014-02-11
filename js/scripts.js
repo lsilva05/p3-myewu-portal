85eb1618fcdd5ee2fc0103c6d91f56cecc0f59ab
Silva <lsilva05@engr-131-08.eastern.ewu.edu>
2014-02-11 14:54:59 -0800

    whooshy whooshy

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).foundation();
+
+$('.box').transition({
+  perspective: '100px',
+  rotate3d: '1,1,0,180deg'
+}, 3000)
+	.transition({
+		x: '100px'
+	})
+
+
+;
+//console.log("ok usa!");
